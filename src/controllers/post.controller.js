79ef2e2569537b19d32918e175790f19e85cc5bd
ArnavAnkush <arnav.ankush.personal@gmail.com>
2026-03-22 21:14:34 +0530

    Implement JWT authentication for post creation and retrival; add error handling for unauthorised access

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -1,6 +1,9 @@
 const postModel = require("../models/post.model")
 const ImageKit = require("@imagekit/nodejs")
 const { toFile } = require("@imagekit/nodejs")
+const jwt = require("jsonwebtoken")
+const postRouter = require("../routes/post.routes")
+
 
 const imagekit = new ImageKit({
     privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
@@ -9,11 +12,105 @@ const imagekit = new ImageKit({
 async function createPostController(req, res){
     console.log(req.body, req.file)
 
+    const token = req.cookies.token
+
+    if (!token){
+        return res.status(401).json({message: "Unauthorized access"})
+    }
+
+    let decoded = null
+
+    try{
+        decoded = jwt.verify(token, process.env.JWT_SECRET)
+    }catch(err){
+        return res.status(401).json({message: "user not authorized"})
+    }
+    
+    console.log(decoded)
+
     const file = await imagekit.files.upload({
         file: await toFile(Buffer.from(req.file.buffer), 'file'),
         fileName: "Test",
+        folder:"cohort-2-instagram-post"
     })
-    res.send(file)
+    
+    const post = await postModel.create({
+        caption: req.body.caption,
+        imgUrl: file.url,
+        user: decoded.id
+    })
+
+    res.status(201).json({message: "Post created successfully", post})
 }
 
-module.exports = { createPostController } 
+async function getPostsController(req, res){
+    const token = req.cookies.token
+
+    if (!token){
+        return res.status(401).json({
+            message: "Unauthorized access"
+        })
+    }
+
+    let decoded
+    
+    try{
+        decoded =jwt.verify(token, process.env.JWT_SECRET)
+    }catch(err){
+        return res.status(401).json({
+            message: "Token invalid"
+        })
+    }
+    const userId  = decoded.id
+    const posts  = await postModel.find({
+        user:userId 
+    })
+
+    res.status(200).json({
+        message: "Posts fetched successfully",
+        posts
+    })
+
+
+}
+
+async function getPostDetailsController(req, res){
+    const token = req.cookies.token
+    if(!token){
+        return res.status(401).json({
+            message: "UnAuthorized Access"
+        })
+    }
+
+    let decoded
+    try{
+        decoded = jwt.verify(token, process.env.JWT_SECRET)
+    }catch(err){
+        return res.status(401).json({
+            message: "Invalid token"
+        })
+    }
+    const userId = decoded.id
+    const postId = req.params.postId
+
+    const post = await postModel.findById(postId)
+    if(!post){
+        return res.status(404).json({
+            message: "Post not found"
+        })
+    }
+    if(post.user.toString() !== userId){
+        return res.status(403).json({
+            message: "You are not authorized to access this post"
+        })
+    }
+    res.status(200).json({
+        message: "Post details fetched successfully",
+        post
+    })
+}
+
+
+
+
+module.exports = { createPostController, getPostsController, getPostDetailsController } 
--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -1,6 +1,9 @@
 const express = require("express")
 const postRouter = express.Router()
+const multer = require("multer")
 const postController = require("../controllers/post.controller")
+
+const upload = multer({storage: multer.memoryStorage()})
 
 /*
 * Post /api/post [protected]
@@ -9,5 +12,18 @@ const postController = require("../controllers/post.controller")
 */ 
 
 postRouter.post("/", postController.createPostController)
+
+/**
+ * Get /api/post/ [protected]
+ */
+
+postRouter.get("/", postController.getPostsController)
+
+/**
+ * Get /api/post/details/:postid
+ * - return an detail about specific post with the id. Also check whether the post belongs to the user that the request is come from
+ */
+
+postRouter.get("/details/:postId", postController.getPostDetailsController)
 
 module.exports = postRouter
